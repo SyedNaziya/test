6fdcf1c00dd05e8d2641633df39f6ae0de9d6357
SyedNaziya <40879769+SyedNaziya@users.noreply.github.com>
2018-07-25 09:43:33 +0530

    Update ps1_2.py

--- ps1_2.py
+++ ps1_2.py
@@ -1,7 +1,7 @@
-def fact(x):
-    if x==1:
+def fact(x1):
+    if x1==1:
         return 1
     else:
-        return (x*fact(x-1))
-n1=int(input())
-print(fact(n1))
+        return (x1*fact(x-1))
+n=int(input())
+print(fact(n))
